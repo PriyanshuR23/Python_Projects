909f3fbb1a3a48bdab75c9ef080cd79775deb4c3
Priyanshu Ranjan <92213284+PriyanshuR23@users.noreply.github.com>
2023-08-03 19:31:25 +0530

    Add files via upload

---
+++ YT_VideoDowloader.py
@@ -0,0 +1,17 @@
+#I Have used Python Code to make a Youtube Video downloader to avoid the hastle of using external online YT downloaders
+
+
+from pytube import YouTube
+
+def Download(link):
+    youtubeObject = YouTube(link)
+    youtubeObject = youtubeObject.streams.get_highest_resolution()
+    try:
+        youtubeObject.download()
+    except:
+        print("An error has occurred")
+    print("Download is completed successfully")
+
+
+link = input("Enter the YouTube video URL: ")
+Download(link)
